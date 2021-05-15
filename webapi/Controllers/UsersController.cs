3ab18ebfe875f4b5cc39cc23c364463d511ce13e
ldejesusvega <ldejesusvega@gmail.com>
2021-05-15 17:39:22 -0500

    Create Users controller, inject dbcontext, create two sincronous call endpoints.

---
+++ webapi/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using webapi.Data;
+using webapi.Entity;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+            return user;
+        }
+
+    }
+}
