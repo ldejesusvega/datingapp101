1e89ace07106f161e4805b2619ff8567476cb6f4
ldejesusvega <ldejesusvega@gmail.com>
2021-05-15 17:57:08 -0500

    Convert actual syncronous endpoints to asyncronous endpoints

--- webapi/Controllers/UsersController.cs
+++ webapi/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using webapi.Data;
 using webapi.Entity;
 
@@ -15,18 +17,30 @@ namespace webapi.Controllers
         {
             _context = context;
         }
+
+        /***
+        syncronous call
+                public ActionResult<IEnumerable<AppUser>> GetUsers()
+                {
+                    var users = _context.Users.ToList();
+                    return users;
+                }
+
+        move to asyncronous call below
+
+        **/
+
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            var users = _context.Users.ToList();
-            return users;
+            return await _context.Users.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            var user = _context.Users.Find(id);
-            return user;
+            return await _context.Users.FindAsync(id);
+
         }
 
     }
