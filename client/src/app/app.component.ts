7c8e9f19a503d726785faa253ea1196a16ad9efa
ldejesusvega <ldejesusvega@gmail.com>
2021-05-15 21:14:19 -0500

    Implements OnInit Add getUsers method to get users list

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  users: any;
+  title = 'The Dating App';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/Users').subscribe(
+      (response) => {
+        console.log(response);
+        this.users = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
